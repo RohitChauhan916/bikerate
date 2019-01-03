a4837f3cec461988bfecdd3b119e1637521724fe
Geek <rohitchauhan916@hotmail.com>
2019-01-04 00:25:45 +0530

    add demo.js

---
+++ demo.js
@@ -0,0 +1,18 @@
+function shadow(){
+  document.getElementById('myImage').src='bike1.jpg';
+}
+function shadowFir(){
+  document.getElementById('myImage').src='bike2.jpg';
+}
+function shadowSec(){
+  document.getElementById('myImage').src='bike3.jpg';
+}
+function shadowThir(){
+  document.getElementById('myImage').src='bike4.jpg';
+}
+function shadowFou(){
+  document.getElementById('myImage').src='bike5.jpg';
+}
+function shadowFif(){
+  document.getElementById('myImage').src='bike6.jpg';
+}
